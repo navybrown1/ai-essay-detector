74175bd9b2ad4ade9f618719d1d5953b2335b19a
Edwin Brown <edwinbrown1@Edwins-Air.lan>
2026-05-23 14:01:30 -0400

    Fix output directory for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, "../dist"),
+  },
   server: {
     port: 3000,
     proxy: {
