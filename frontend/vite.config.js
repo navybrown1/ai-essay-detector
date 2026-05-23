ef9371b0a50d8a00beb5063308953a8ff13a4ab3
Edwin Brown <edwinbrown1@Edwins-Air.lan>
2026-05-23 14:01:55 -0400

    Simplify output directory path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,11 @@ import react from "@vitejs/plugin-react";
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, "../dist"),
+    outDir: "../dist",
   },
   server: {
     port: 3000,
